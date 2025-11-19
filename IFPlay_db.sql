b57df4e05ff97d58e966d75778205e64670d11a9
dorywill <ramdsilvers@gmail.com>
2025-11-19 15:01:47 -0300

    banco

---
+++ IFPlay_db.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE IFPlay_db;
+USE IFPlay_db;
+CREATE TABLE aluno(
+    alunoID INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(150) NOT NULL UNIQUE,
+    matricula VARCHAR(25) NOT NULL UNIQUE,
+    anoEntrada DATE NOT NULL UNIQUE,
+    status BOOLEAN DEFAULT 0
+);
+
+CREATE TABLE frequencia_atividade(
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    descricao VARCHAR(400),
+    data DATE NOT NULL,
+    horario NOT NULL TIME;
+    situação VARCHAR(25),
+    participante INT NOT NULL,
+    FOREIGN KEY (participante) REFERENCES aluno(alunoID)
+);
