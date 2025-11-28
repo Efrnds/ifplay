dcaac908a5a55b05b8a0ae9f16557f199187f3d9
Pedro Rodrigues <pedrorodriguesssrrr@gmail.com>
2025-11-28 20:31:13 -0300

    paginacao

--- utils/conexao.php
+++ utils/conexao.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "127.0.0.1";
-$username = "eduardo";
-$password = "123456";
+$username = "root";
+$password = "";
 $dbname = "IFPlay_db";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
